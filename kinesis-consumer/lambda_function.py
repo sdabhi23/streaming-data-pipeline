34d2d566a1fee63d5403f0ab10238b578c56890b
shrey <shrey.dabhi23@gmail.com>
2024-03-11 23:45:00 +0530

    add transformation code

--- kinesis-consumer/lambda_function.py
+++ kinesis-consumer/lambda_function.py
@@ -2,8 +2,37 @@ import json
 import base64
 
 
-def lambda_handler(event, context):
-    for event in event["Records"]:
-        print(base64.b64decode(event["kinesis"]["data"]).decode("utf-8"))
+class FirehoseOutputRecordStatuses:
+    OK = "Ok"
+    DROPPED = "Dropped"
+    PROCESSING_FAILED = "ProcessingFailed"
 
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+
+def lambda_handler(event, context):
+    output = []
+
+    for record in event["records"]:
+        payload = json.loads(base64.b64decode(record["data"]).decode("utf-8"))
+        result = FirehoseOutputRecordStatuses.OK
+
+        try:
+            payload["user"]["user_id"] = int(payload["user"]["user_id"])
+        except Exception:
+            # catch any issues and send them off to the error queue / bucket from Firehose
+            result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+
+        output_record = {
+            "partitionKey": record["partitionKey"],
+            "sequenceNumber": record["sequenceNumber"],
+            "approximateArrivalTimestamp": record["approximateArrivalTimestamp"],
+            "subSequenceNumber": record["subSequenceNumber"],
+            "action": record["action"],
+            "kinesisRecordMetadata": record["kinesisRecordMetadata"],
+            "result": result,
+            "data": base64.b64encode((json.dumps(payload) + "\n").encode("utf-8")).decode("utf-8"),
+        }
+        output.append(output_record)
+
+    print("Successfully processed {} records.".format(len(event["records"])))
+
+    return {"records": output}
