7154140cbf4af4c0210cc4cf48e737594861871c
shrey <shrey.dabhi23@gmail.com>
2024-03-12 23:19:31 +0530

    update data transformation function to handle records correctly

--- kinesis-consumer/lambda_function.py
+++ kinesis-consumer/lambda_function.py
@@ -1,6 +1,7 @@
 import json
 import base64
 import logging
+import binascii
 
 
 class FirehoseOutputRecordStatuses:
@@ -9,37 +10,73 @@ class FirehoseOutputRecordStatuses:
     PROCESSING_FAILED = "ProcessingFailed"
 
 
+def log_error(msg, record_id, kinesis_metadata):
+    print(f"Record ID: {record_id}")
+    print(f"Shard ID: {kinesis_metadata['shardId']}")
+    print(f"Sequence Number: {kinesis_metadata['sequenceNumber']}")
+    logging.exception(msg)
+
+
 def lambda_handler(event, context):
     output = []
 
     for record in event["records"]:
-        payload = json.loads(base64.b64decode(record["data"]).decode("utf-8"))
         result = FirehoseOutputRecordStatuses.OK
+        payload = ""
 
+        # catch any issues and send the records to the error queue / bucket from Firehose
         try:
-            payload["user"]["user_id"] = int(payload["user"]["user_id"])
-        except Exception:
-            logging.exception("Error parsing 'user.user_id' as an integer")
-            # catch any issues and send them off to the error queue / bucket from Firehose
-            result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+            json_string = base64.b64decode(record["data"]).decode("utf-8")
+            payload = json.loads(json_string)
 
-        try:
-            payload["referral"]["user_id"] = int(payload["referral"]["user_id"])
-        except Exception:
-            logging.exception("Error parsing 'referral.user_id' as an integer")
-            # catch any issues and send them off to the error queue / bucket from Firehose
+            try:
+                payload["user"]["user_id"] = int(payload["user"]["user_id"])
+            except Exception:
+                log_error(
+                    "Error parsing 'user.user_id' as an integer",
+                    record["recordId"],
+                    record["kinesisRecordMetadata"],
+                )
+                result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+
+            try:
+                payload["referral"]["user_id"] = int(payload["referral"]["user_id"])
+            except Exception:
+                log_error(
+                    "Error parsing 'referral.user_id' as an integer",
+                    record["recordId"],
+                    record["kinesisRecordMetadata"],
+                )
+                result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+
+            output_data = base64.b64encode(
+                (json.dumps(payload) + "\n").encode("utf-8")
+            ).decode("utf-8")
+        except json.JSONDecodeError:
+            log_error(
+                "Error decoding JSON string",
+                record["recordId"],
+                record["kinesisRecordMetadata"],
+            )
             result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+            output_data = base64.b64encode((json_string + "\n").encode("utf-8")).decode(
+                "utf-8"
+            )
+        except binascii.Error:
+            log_error(
+                "Error decoding the record from base64 format",
+                record["recordId"],
+                record["kinesisRecordMetadata"],
+            )
+            result = FirehoseOutputRecordStatuses.PROCESSING_FAILED
+            output_data = record["data"]
 
         output_record = {
-            "partitionKey": record["partitionKey"],
-            "sequenceNumber": record["sequenceNumber"],
-            "approximateArrivalTimestamp": record["approximateArrivalTimestamp"],
-            "subSequenceNumber": record["subSequenceNumber"],
-            "action": record["action"],
-            "kinesisRecordMetadata": record["kinesisRecordMetadata"],
+            "recordId": record["recordId"],
             "result": result,
-            "data": base64.b64encode((json.dumps(payload) + "\n").encode("utf-8")).decode("utf-8"),
+            "data": output_data,
         }
+
         output.append(output_record)
 
     print("Successfully processed {} records.".format(len(event["records"])))
